38836ef7301487f1ae8426fc86289b8a4f71c359
Gabor Somogyi <gabor.somogyi@atolldev.hu>
2021-12-03 10:07:27 +0100

    Add basic configuration (from inner resource)

--- microservices-demo-web/src/main/java/hu/gsomogyi/microservicesdemo/MicroservicesDemoApplication.java
+++ microservices-demo-web/src/main/java/hu/gsomogyi/microservicesdemo/MicroservicesDemoApplication.java
@@ -3,7 +3,6 @@ package hu.gsomogyi.microservicesdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class MicroservicesDemoApplication {
 
--- microservices-demo-web/src/main/java/hu/gsomogyi/microservicesdemo/controller/LimitController.java
+++ microservices-demo-web/src/main/java/hu/gsomogyi/microservicesdemo/controller/LimitController.java
@@ -1,6 +1,8 @@
 package hu.gsomogyi.microservicesdemo.controller;
 
+import hu.gsomogyi.microservicesdemo.Configuration;
 import hu.gsomogyi.microservicesdemo.model.Limit;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +13,11 @@ public class LimitController {
 
     static final String BASE_URL = "/api/limits";
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping
     public Limit retrieveLimits() {
-        return new Limit(1, 1000);
+        return new Limit(configuration.getMinimum(), configuration.getMaximum());
     }
 }
